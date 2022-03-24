4d825c7b54ae01b259a565c6ec6e64fd23dfa070
Airton Braga Amorim <airtonbragaee@gmail.com>
2022-03-24 12:39:41 -0300

    adicao de comentarios explicativos em sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -15,18 +15,29 @@ interface ISideBarProps {
   handleClickButton(id: number): void;
 }
 
+/*  O componente Sidebar recebe como parameto os objetos javascript"selectedGeneroId" e a funcao "handleClickButton", perceba  o termo "ISideBarProps"
+que Orienta a aplicacao  quanto ao formato(typagem) desses parametros recebidos   */
 export function SideBar({
   selectedGenreId,
-  handleClickButton
-}: ISideBarProps) {
+  handleClickButton,
+}: ISideBarProps)
+   //Inicio das  Intrucoes da funcao(do componente "Sidebar")
+ {
+   // Atribuindo  o Hook "useState" a variavel genres e retorna uma Array 
   const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-
+  //  Aqui o Hook "useEffect"(uma funcao tambem,pra variar)
+  /* Muito  importante: note que a funcao recebe como parametros,uma outra funcao(callback.no caso uma arrayfunction) e 
+  uma array de depencias que informa qd essa funcao serar executada, perceba pelo simbolo de "[]" 
+  note que interessante: A funcao "useEffetct ela nao tem de forma "explicita  instrucoes ,ela só recebe parametros ,as intrucoes do que deve ser feito ,está na ArrowFunction
+  no caso Serar execultado só uma vez*/
   useEffect(() => {
     api.get<GenreResponseProps[]>('genres').then(response => {
       setGenres(response.data);
     });
   }, []);
 
+
+  // Retorno do componente "SideBar"
   return (
     <nav className="sidebar">
       <span>Watch<p>Me</p></span>
